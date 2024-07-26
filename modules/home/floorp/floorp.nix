1807d2f1925f8e7c4523e01294df385733d75e08
Luca <luca.antonelli@gmx.ch>
2024-07-26 18:56:23 +0200

    changed syntax

--- modules/home/floorp/floorp.nix
+++ modules/home/floorp/floorp.nix
@@ -1,7 +1,19 @@
-{ pkgs, ... }: 
+{ pkgs, username, ... }: 
 {
   programs.firefox = {
     enable = true;
     package = pkgs.floorp;
+
+#    profiles = {
+ #     default = {
+  #      id = 0;
+   #     name = "luca";
+    #    isDefault = true;
+     #   extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+          # nordpass
+      #    ublock-origin
+       # ];
+#      };
+ #   };
   };
 }
