3d497db0be6ed866ce51763218719b219d51b92a
Luca <luca.antonelli@gmx.ch>
2024-07-26 17:25:01 +0200

    tweaked desktop config to hopefully fix it

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -6,4 +6,16 @@
   ];
 
   powerManagement.cpuFreqGovernor = "performance";
+
+  # Possible fix for desktop: Specify NVIDIA driver
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    powerManagement.finegrained = false;
+
+    nvidiaSettings = true;
+  };
 }
