50d5ce555eeba2a6a69f0bf33944de337bb362b8
Luca <luca.antonelli@gmx.ch>
2024-07-29 17:10:57 +0200

    added options from official NixOS wiki for Nvidia GPUs for desktop

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -6,5 +6,23 @@
   ];
 
   powerManagement.cpuFreqGovernor = "performance";
+  hardware.opengl = {
+    enable = true;
+  };
 
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+
+    powerManagement.enable = false;
+
+    powerManagement.finegrained = false;
+
+    open = false;
+
+    nvidiaSettings = true;
+
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 }
