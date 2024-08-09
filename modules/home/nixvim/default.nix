69c8e5d710ecd6f971117f275e6fe4592e35ab3c
Luca <luca.antonelli@gmx.ch>
2024-08-09 14:11:30 +0200

    added plugins from NvChad plugin list

--- modules/home/nixvim/default.nix
+++ modules/home/nixvim/default.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }: {
+{ inputs, pkgs, ... }: {
 
   imports = [ 
     inputs.nixvim.homeManagerModules.nixvim 
@@ -15,10 +15,24 @@
 	clipboard = "unnamedplus";
     };  
     plugins = {
-	bufferline.enable = true;
-	oil.enable = true;
-	lualine.enable = true;
-	nvim-autopairs.enable = true;
+      bufferline.enable = true;
+      oil.enable = true;
+      lualine.enable = true;
+      nvim-autopairs.enable = true;
+      nvim-tree.enable = true;
+      gitsigns.enable = true;
+      cmp.enable = true;
+      telescope.enable = true;
+      treesitter.enable = true;
+      indent-blankline.enable = true;
+      friendly-snippets.enable = true;
+      luasnip.enable = true;
+      which-key.enable = true;     
     };
+    extraPlugins = with pkgs.vimPlugins; [
+      nvim-web-devicons
+      nvim-lspconfig
+      mason-nvim
+    ];
   };
 }
