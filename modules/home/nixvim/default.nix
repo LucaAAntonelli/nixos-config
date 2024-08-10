4f30c2faa1ce160fe8313410182ff904167d460d
Luca <luca.antonelli@gmx.ch>
2024-08-11 01:41:56 +0200

    added first batch of NvChad key bindings to NixVim

--- modules/home/nixvim/default.nix
+++ modules/home/nixvim/default.nix
@@ -34,5 +34,128 @@
       nvim-lspconfig
       mason-nvim
     ];
+
+    keymaps = [
+      {
+        action = "<ESC>^i";
+        key = "<C-b>";
+        mode = "i";
+        options.desc = "move beginning of line";
+      } 
+      {
+        action = "<End>";
+        key = "<C-e>";
+        mode = "i";
+        options.desc = "move end of line";
+      }
+      {
+        action = "<Left>";
+        key = "<C-h>";
+        mode = "i";
+        options.desc = "move left";
+      }
+      {
+        action = "<Right>";
+        key = "<C-l>";
+        mode = "i";
+        options.desc = "move right";
+      }
+      {
+        action = "<Down>";
+        key = "<C-j>";
+        mode = "i";
+        options.desc = "move down";
+      }
+      {
+        action = "<Up>";
+        key = "<C-k>";
+        mode = "i";
+        options.desc = "move up";
+      }
+      {
+        action = "<cmd>noh<CR>";
+        key = "<Esc>";
+        mode = "n";
+        options.desc = "general clear highlights";
+      }
+      {
+        action = "<C-w>h";
+        key = "<C-h>";
+        mode = "n";
+        options.desc = "switch window left";
+      }
+      {
+        action = "<C-w>l";
+        key = "<C-l>";
+        mode = "n";
+        options.desc = "switch window right";
+      }
+      {
+        action = "<C-w>j";
+        key = "<C-j>";
+        mode = "n";
+        options.desc = "switch window down";
+      }
+      {
+        action = "<C-w>k";
+        key = "<C-k>";
+        mode = "n";
+        options.desc = "switch window up";
+      }
+      {
+        action = "<cmd>w<CR>";
+        key = "<C-s>";
+        mode = "n";
+        options.desc = "file save";
+      }
+      {
+        action = "<cmd>%y+<CR>";
+        key = "<C-c>";
+        mode = "n";
+        options.desc = "file copy whole";
+      }
+      {
+        action = "<cmd>set nu!<CR>";
+        key = "<leader n>";
+        mode = "n";
+        options.desc = "toggle line number";
+      }
+      {
+        action = "<cmd>set rnu!<CR>";
+        key = "<leader>rn";
+        mode = "n";
+        options.desc = "toggle relative number";
+      }
+      {
+        action = "<cmd>NvCheatsheet<CR>";
+        key = "<leader>ch";
+        mode = "n";
+        options.desc = "toggle nvcheatsheet";
+      }
+      {
+        action = '' 
+          function()
+          require("conform").format {lsp_fallback=true}
+          end
+        '';
+        key = "<leader>fm";
+        mode = "n";
+        options.desc = "format files";
+      }
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+    ];
   };
 }
