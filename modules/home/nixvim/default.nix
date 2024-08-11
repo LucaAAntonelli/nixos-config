7d114d71dbac8573fda3d17405de671a81b4096a
Luca <luca.antonelli@gmx.ch>
2024-08-11 19:03:54 +0200

    added NvChad options

--- modules/home/nixvim/default.nix
+++ modules/home/nixvim/default.nix
@@ -8,11 +8,38 @@
     enable = true;
     vimAlias = true;
     colorschemes.gruvbox.enable = true;
+    extraConfigLua = ''
+      vim.opt.whichwrap:append("<>[]hl")
+    '';
+    globals = {
+      mapleader = " ";
+    };
     opts = {
     	number = true;
-    	shiftwidth = 4;
-	
-	clipboard = "unnamedplus";
+    	shiftwidth = 2;
+	    clipboard = {
+        providers.wl-copy.enable = true;
+      };
+      laststatus = 3;
+      showmode = false;
+      cursorline = true;
+      cursorlineopt = "number";
+
+      expandtab = true;
+      smartindent = true;
+      tabstop = 2;
+      softtabstop = 2;
+      ignorecase = true;
+      smartcase = true;
+      mouse = "a";
+      numberwidth = 2;
+      ruler = false;
+      signcolumn = "yes";
+      splitbelow = true;
+      splitright = true;
+      timeoutlen = 400;
+      undofile = true;
+      updatetime = 250;
     };  
     plugins = {
       bufferline.enable = true;
