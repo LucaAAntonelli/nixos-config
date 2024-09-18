4b992e31d9db249d3ec2667389bf9949bffd9af3
Luca <luca.antonelli@gmx.ch>
2024-09-18 10:43:19 +0200

    added printing capabilities

--- modules/core/services.nix
+++ modules/core/services.nix
@@ -5,6 +5,12 @@
     gnome.gnome-keyring.enable = false;
     dbus.enable = true;
     fstrim.enable = true;
+    printing.enable = true;
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
   };
   services.logind.extraConfig = ''
     # don’t shutdown when power button is short-pressed
