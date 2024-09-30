aac17566a71052e6a9f8cdef31b799dda19a9ed1
Luca <luca.antonelli@gmx.ch>
2024-09-30 21:34:42 +0200

    moved bootloader entry as conditional into bootloader.nix

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -7,7 +7,19 @@
     efiSupport = true;
     efiInstallAsRemovable = false;
     useOSProber = false;
-    extraEntries = builtins.import ./../../hosts/${host}/windows-boot-entry.nix;
+    extraEntries = if host == "desktop" then ''
+      menuentry 'Windows Boot Manager (on /dev/sdb2)' --class windows --class os $menuentry_id_option 'osprober-efi-10A6-35AF' {
+	      insmod part_gpt
+	      insmod fat
+	      set root='hd1,gpt2'
+	      if [ x$feature_platform_search_hint = xy ]; then
+	        search --no-floppy --fs-uuid --set=root --hint-bios=hd1,gpt2 --hint-efi=hd1,gpt2 --hint-baremetal=ahci1,gpt2  10A6-35AF
+	      else
+	        search --no-floppy --fs-uuid --set=root 10A6-35AF
+	      fi
+	      chainloader /efi/Microsoft/Boot/bootmgfw.efi
+      }
+    '' else "";
   };
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
