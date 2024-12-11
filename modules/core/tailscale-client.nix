33e3cf9a30f641d05a931d44f755fa501d3dd471
Luca <luca.antonelli@gmx.ch>
2024-12-11 23:08:21 +0100

    added tailscale client

---
+++ modules/core/tailscale-client.nix
@@ -0,0 +1,8 @@
+{ ... }: {
+  services.tailscale.enable = true;
+  networking.firewall = {
+    checkReversePath = "loose";
+    trustedInterfaces = [ "tailscale0" ];
+    allowedUDPPorts = [ 8080 ];
+    };
+  }
