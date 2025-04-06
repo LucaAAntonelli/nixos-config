54b825c5ad53cebc640d9405f2c3beba4212a845
Luca Antonelli <luca.antonelli@gmx.ch>
2025-04-06 21:50:21 +0200

    adjusted new option definitions, updated to nextcloud 31

--- modules/core/services/nextcloud.nix
+++ modules/core/services/nextcloud.nix
@@ -2,17 +2,20 @@
 {
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud30;
+    package = pkgs.nextcloud31;
     hostName = "nextcloud.${inputs.secrets.domain}";
-    config.adminuser = username;
-    config.adminpassFile = config.sops.secrets.nextcloud-admin-pass.path;
+    config = { 
+      adminuser = username;
+      adminpassFile = config.sops.secrets.nextcloud-admin-pass.path;
+      dbtype = "mysql";
+    };
     database.createLocally = true;
     configureRedis = true;
     maxUploadSize = "16G";
     https = true;
     extraAppsEnable = true;
     extraApps = {
-      inherit (pkgs.nextcloud30Packages.apps) onlyoffice;
+      inherit (pkgs.nextcloud31Packages.apps) onlyoffice;
     };
   };
 }
