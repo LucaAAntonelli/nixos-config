13f04a0c11770447b63ffeee4f7ae2cf753fc588
Luca Antonelli <luca.antonelli@gmx.ch>
2025-05-23 18:07:38 +0200

    override python dependencies: include jsonschema to enable kobo sync

--- modules/core/services/calibre-web.nix
+++ modules/core/services/calibre-web.nix
@@ -1,6 +1,19 @@
-{ ... }: {
+{pkgs, ... }: 
+  let
+    calibre-web-with-kobo = pkgs.calibre-web.overridePythonAttrs (oldAttrs: {
+      dependencies = oldAttrs.dependencies ++ [ pkgs.python3Packages.jsonschema];
+      });
+  in 
+  {
   services.calibre-web = {
     enable = true;
+    package = calibre-web-with-kobo;
+    options.enableBookUploading = true;
+    options.enableKepubify = true;
+    listen = {
+      ip = "192.168.1.212";
+      port = 8083;
+    };
 
   };
 }
