cce365a1a04fa56c7e102b1fdb519aee292e41dd
Luca Antonelli <luca.antonelli@gmx.ch>
2025-08-29 20:06:13 +0200

    use nix secrets to hide syncthing IDs

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -1,12 +1,27 @@
-{ ... }: {
+{ inputs, ... }: {
   services.syncthing = {
     enable = true;
     openDefaultPorts = true;
     extraFlags = [ "--no-default-folder" ];
+    user = "luca";
+    configDir =
+      "/home/luca/.config/syncthing"; # set to somewhere in home directory to have access with user
 
-    settings.gui = {
-      user = "myuser";
-      password = "mypassword";
+    settings = {
+      devices = {
+        "Desktop-Windows" = { id = inputs.secrets.syncthing-ids.windows; };
+        "Phone" = { id = inputs.secrets.syncthing-ids.phone; };
+      };
+      folders = {
+        "Library" = {
+          path = "/home/luca/library";
+          devices = [ "Desktop-Windows" ];
+        };
+        "Audibooks" = {
+          path = "/home/luca/audiobooks";
+          devices = [ "Desktop-Windows" ];
+        };
+      };
     };
   };
 }
