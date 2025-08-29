276809c142884dbfb9dc945250958a1704c315d5
Luca Antonelli <luca.antonelli@gmx.ch>
2025-08-29 20:06:50 +0200

    use user luca to have access to home directory

--- modules/services/calibre-web.nix
+++ modules/services/calibre-web.nix
@@ -1,15 +1,16 @@
-{pkgs, ... }: 
-  let
-    calibre-web-with-kobo = pkgs.calibre-web.overridePythonAttrs (oldAttrs: {
-      dependencies = oldAttrs.dependencies ++ [ pkgs.calibre-web.optional-dependencies.kobo];
-      });
-  in 
-  {
+{ pkgs, ... }:
+let
+  calibre-web-with-kobo = pkgs.calibre-web.overridePythonAttrs (oldAttrs: {
+    dependencies = oldAttrs.dependencies
+      ++ [ pkgs.calibre-web.optional-dependencies.kobo ];
+  });
+in {
   services.calibre-web = {
     enable = true;
     package = calibre-web-with-kobo;
     options.enableBookUploading = true;
     options.enableKepubify = true;
+    user = "luca"; # allow access to /home/luca/library
     listen = {
       ip = "192.168.1.212";
       port = 8083;
