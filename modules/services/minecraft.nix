009aaee7d54ef2275f863efacd4e5814c462502b
Luca Antonelli <luca.antonelli@gmx.ch>
2025-08-30 23:32:46 +0200

    formatting

--- modules/services/minecraft.nix
+++ modules/services/minecraft.nix
@@ -1,12 +1,13 @@
-{inputs, lib, ... }:
-{
-  imports = [inputs.mms.module];
+{ inputs, lib, ... }: {
+  imports = [ inputs.mms.module ];
   services.modded-minecraft-servers = {
     eula = true;
     instances = {
       divinejourney2 = {
         enable = false; # disable for now
-        rsyncSSHKeys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFKjNIRdnej5xu7x68O7Yj5Svz4QZW7C60gCCDs/Hfni"];
+        rsyncSSHKeys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFKjNIRdnej5xu7x68O7Yj5Svz4QZW7C60gCCDs/Hfni"
+        ];
 
         # Currently not used, but left here in case this changes
         jvmOpts = lib.concatStringsSep " " [
@@ -18,7 +19,7 @@
           "-XX:+AlwaysPreTouch"
           "-XX:G1NewSizePercent=40"
           "-XX:G1MaxNewSizePercent=50"
-#          "-XX:G1HeapRegionSize=16M"
+          #          "-XX:G1HeapRegionSize=16M"
           "-XX:G1ReservePercent=15"
           "-XX:G1HeapWastePercent=5"
           "-XX:G1MixedGCCountTarget=4"
@@ -29,7 +30,6 @@
           "-XX:+PerfDisableSharedMem"
           "-XX:MaxTenuringThreshold=1"
         ];
-
 
         serverConfig = {
           motd = "NixOS-hosted Divine Journey 2 server";
