2f178d2c1d81fb6a8d47f5ec7d7d99c741f03ed1
Luca <luca.antonelli@gmx.ch>
2025-08-30 13:53:09 +0200

    added laptop and homelab syncthing IDs, made it so devices don't reference their own IDs

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -1,4 +1,15 @@
-{ inputs, ... }: {
+{ inputs, host, lib, ... }:
+let
+  allDevices = {
+    "Desktop-Windows" = { id = inputs.secrets.syncthing-ids.windows; };
+    "phone" = { id = inputs.secrets.syncthing-ids.phone; };
+    "laptop" = { id = inputs.secrets.syncthing-ids.laptop; };
+    "homelab" = { id = inputs.secrets.syncthing-ids.homelab; };
+  };
+  peers = lib.removeAttrs allDevices
+    [ host ]; # avoids referencing only other devices for folders
+  peerNames = builtins.attrNames peers;
+in {
   services.syncthing = {
     enable = true;
     openDefaultPorts = true;
@@ -8,18 +19,15 @@
       "/home/luca/.config/syncthing"; # set to somewhere in home directory to have access with user
 
     settings = {
-      devices = {
-        "Desktop-Windows" = { id = inputs.secrets.syncthing-ids.windows; };
-        "Phone" = { id = inputs.secrets.syncthing-ids.phone; };
-      };
+      devices = peers;
       folders = {
         "Library" = {
           path = "/home/luca/library";
-          devices = [ "Desktop-Windows" ];
+          devices = peerNames;
         };
         "Audibooks" = {
           path = "/home/luca/audiobooks";
-          devices = [ "Desktop-Windows" ];
+          devices = peerNames;
         };
       };
     };
