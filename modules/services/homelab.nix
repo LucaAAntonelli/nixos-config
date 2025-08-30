5e3f1ce43bd9c54455f11fe2d5192c3dd3b53a70
Luca <luca.antonelli@gmx.ch>
2025-08-30 12:54:54 +0200

    add default.nix and homelab.nix to services for proper inclusion

---
+++ modules/services/homelab.nix
@@ -0,0 +1,16 @@
+{...}:
+{
+  imports = [
+    ./nginx.nix
+    ./immich.nix
+    ./fail2ban.nix
+    ./nextcloud.nix
+    ./onlyoffice.nix
+    ./vaultwarden.nix
+    ./cloudflared.nix
+    ./minecraft.nix
+    ./calibre-web.nix
+    ./audiobookshelf.nix
+    ./syncthing.nix
+  ];
+}
