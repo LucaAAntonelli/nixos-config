a536dd38b5ce21c40b60ef09357846fd211bc7d1
Luca Antonelli <luca.antonelli@gmx.ch>
2025-08-31 20:56:17 +0200

    added fd to homelab home-manager config, needed in nvim file find

--- modules/home/homelab.nix
+++ modules/home/homelab.nix
@@ -1,22 +1,12 @@
-{ pkgs, username, ... }: 
-{
-  imports = [
-    ./bat.nix
-    ./nvim.nix
-    ./tmux.nix
-    ./starship.nix
-    ./zsh.nix
-    ./git.nix
-  ];
+{ pkgs, username, ... }: {
+  imports =
+    [ ./bat.nix ./nvim.nix ./tmux.nix ./starship.nix ./zsh.nix ./git.nix ];
   nixpkgs.config.allowUnfree = true;
   home = {
     username = "${username}";
     homeDirectory = "/home/${username}";
     stateVersion = "24.05";
-    packages = (with pkgs; [
-      fzf
-      direnv
-    ]);
+    packages = (with pkgs; [ fzf direnv fd ]);
   };
   programs.home-manager.enable = true;
 }
