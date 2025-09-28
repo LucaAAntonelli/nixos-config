a15e979e77dee1119b5711e26ab9a30c49d9b911
Luca <luca.antonelli@gmx.ch>
2025-09-28 18:07:48 +0200

    add direnv support to nushell

--- modules/home/nu.nix
+++ modules/home/nu.nix
@@ -3,6 +3,21 @@
     nushell = {
       enable = true;
       settings = { show_banner = false; };
+      extraConfig = ''
+        $env.config = {
+          hooks: {
+            pre_prompt: [{ ||
+              if (which direnv | is-empty) {
+                return
+              }
+
+              direnv export json | from json | default {} | load-env
+              if 'ENV_CONVERSIONS' in $env and 'PATH' in $env.ENV_CONVERSIONS {
+                $env.PATH = do $env.ENV_CONVERSIONS.PATH.from_string $env.PATH
+              }
+            }]
+          }
+        }'';
       shellAliases = {
 
         # Utils
